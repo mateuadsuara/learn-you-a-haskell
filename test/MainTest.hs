a442fdf942787c662a05d41e2665c7fec7a540dd
Mateu Adsuara <mateuadsuara@gmail.com>
2020-09-15 16:25:36 +0200

    Property test and trace head'

--- test/MainTest.hs
+++ test/MainTest.hs
@@ -1,4 +1,16 @@
 module Main (main) where
 
+import ListFunctions
+
+import Test.Hspec
+import Test.QuickCheck
+
+import Debug.Trace
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = hspec $ do
+  describe "head'" $ do
+    it "works as head" $
+      property $ \xs ->
+        trace ("checking head' and head return the same for " ++ show xs) $
+        xs == [] || (head xs) == (head' xs :: Int)
