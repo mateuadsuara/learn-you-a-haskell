e811197d63227da453493aa05086354d1bbb594f
Mateu Adsuara <mateuadsuara@gmail.com>
2020-09-16 02:23:40 +0200

    Add shouldThrowError for simpler expectations

--- test/MainTest.hs
+++ test/MainTest.hs
@@ -12,11 +12,15 @@ main :: IO ()
 main = hspec $ do
   describe "head'" $ do
     it "throws an exception if used with an empty list like head does" $ do
-      evaluate (head [])  `shouldThrow` errorCall "Prelude.head: empty list"
-      evaluate (head' []) `shouldThrow` errorCall "empty list"
+      (head [])  `shouldThrowError` "Prelude.head: empty list"
+      (head' []) `shouldThrowError` "empty list"
     it "works as head for non-empty lists" $
       property $ forAll nonEmptyLists $ \xs ->
         trace ("checking head' and head return the same for " ++ show xs) $
         (head xs) == (head' xs)
         where nonEmptyLists :: Gen [Int]
               nonEmptyLists = (arbitrary `suchThat` (not . null))
+
+shouldThrowError :: a -> String -> IO ()
+shouldThrowError result expectedErrorDescription =
+  evaluate result `shouldThrow` errorCall expectedErrorDescription
