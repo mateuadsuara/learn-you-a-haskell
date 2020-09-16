f1df32319e216cc18d3b51ff9c0e29947b841226
Mateu Adsuara <mateuadsuara@gmail.com>
2020-09-16 02:08:34 +0200

    Add assertion that head' [] throws error as head does

--- test/MainTest.hs
+++ test/MainTest.hs
@@ -4,12 +4,16 @@ import ListFunctions
 
 import Test.Hspec
 import Test.QuickCheck
+import Control.Exception (evaluate)
 
 import Debug.Trace
 
 main :: IO ()
 main = hspec $ do
   describe "head'" $ do
+    it "throws an exception if used with an empty list like head does" $ do
+      evaluate (head [])  `shouldThrow` errorCall "Prelude.head: empty list"
+      evaluate (head' []) `shouldThrow` errorCall "empty list"
     it "works as head for non-empty lists" $
       property $ forAll nonEmptyLists $ \xs ->
         trace ("checking head' and head return the same for " ++ show xs) $
