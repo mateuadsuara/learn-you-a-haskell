f2e5ec62c87a98c28dd5af530ad3296786db4b8a
Mateu Adsuara <mateuadsuara@gmail.com>
2020-09-16 02:26:47 +0200

    Remove trace

--- test/MainTest.hs
+++ test/MainTest.hs
@@ -6,8 +6,6 @@ import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
 
-import Debug.Trace
-
 main :: IO ()
 main = hspec $ do
   describe "head'" $ do
@@ -16,7 +14,6 @@ main = hspec $ do
       (head' []) `shouldThrowError` "empty list"
     it "works as head for non-empty lists" $
       property $ forAll nonEmptyLists $ \xs ->
-        trace ("checking head' and head return the same for " ++ show xs) $
         (head xs) == (head' xs)
         where nonEmptyLists :: Gen [Int]
               nonEmptyLists = (arbitrary `suchThat` (not . null))
